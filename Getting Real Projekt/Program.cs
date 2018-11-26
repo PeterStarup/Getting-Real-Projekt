d4782895106c12976ad5a2625abffa8edf714b19
Peter Starup <pete59f1@edu.eal.dk>
2018-11-26 14:10:41 +0100

    Add project files.

---
+++ Getting Real Projekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_Real_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
