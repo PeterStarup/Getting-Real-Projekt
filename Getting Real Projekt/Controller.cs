cb7dc0e2a613e9715bed2598c14bd47753e90e4d
Michal Tomasz Odzienkowski <mich040m@edu.eal.dk>
2018-11-28 13:26:04 +0100

    Oprettet SQLController class med metoderne

--- Getting Real Projekt/Controller.cs
+++ Getting Real Projekt/Controller.cs
@@ -19,7 +19,7 @@ namespace Getting_Real_Projekt
         }
         public void InsertEntryAndReservation(string name, string tlfNumber, DateTime timeOfReservation, DateTime date)
         {
-            Sqlcontroller.InsertEntryAndReservation(name, tlfNumber, timeOfReservation);
+            Sqlcontroller.InsertReservation(name, tlfNumber, timeOfReservation);
             Sqlcontroller.InsertEntry(date);
         }
         public void ReadData()
---
+++ Getting Real Projekt/SQLController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_Real_Projekt
+{
+    public class SQLController
+    {
+        public void InsertReservation(string name, string tlfNumber, DateTime timeOfReservation)
+        {
+
+        }
+        public void InsertEntry(DateTime date)
+        {            
+        }
+        public void ReadData()
+        {           
+        }
+        public void ReadSpecificData(string row)
+        {
+        }
+    }
+}
